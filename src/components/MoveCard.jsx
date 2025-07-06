92c1f86a645980268bde5463d929a56bd16cb193
Daehee Han <gkseogml4959@gmail.com>
2025-07-06 10:26:53 +0000

    Revert "수 설명 편집 기능 추가"
    This reverts commit 1a98a734509c963ff21f088195202e006ccc6da9.

--- src/components/MoveCard.jsx
+++ src/components/MoveCard.jsx
@@ -1,34 +1,62 @@
-import EditButton from "./EditButton";
+import { overlay } from "overlay-kit";
+import Modal from "./Modal";
+import { useState } from "react";
 
-function MoveCard({
-  move,
-  onClick,
-  title,
-  description,
-  isEditable,
-  fen,
-  setDb,
-}) {
-  function editMove(newTitle, newDescription) {
-    console.log(newTitle, newDescription);
-    setDb((prevDb) => {
-      const updatedDb = new Map(prevDb);
-      const list = updatedDb.get(fen) || [];
-      const filteredList = list.filter((e) => e.move !== move);
-      filteredList.push({ move, title: newTitle, description: newDescription });
-      updatedDb.set(fen, filteredList);
-      console.log(updatedDb);
-      return updatedDb;
-    });
-  }
-
+function MoveCard({ move, onClick, title, description, isEditable, fen, db }) {
   return (
     <div
       className="w-full flex flex-col p-4 rounded border border-amber-800 h-32 mb-4 cursor-pointer hover:bg-amber-800 hover:text-white hover:border-white relative"
       onClick={() => onClick()}
     >
       {isEditable && (
-        <EditButton title={title} description={description} onSave={editMove} />
+        <button
+          className="absolute right-4 z-100 bg-blue-400 hover:bg-blue-600 text-white rounded px-2 py-1 hover:cursor-pointer"
+          onClick={(e) => {
+            e.stopPropagation();
+            overlay.open(({ isOpen, close }) => (
+              <Modal isOpen={isOpen}>
+                <div className="p-4 rounded border-2 border-amber-800 bg-white h-2/3 w-1/2 relative">
+                  <div className="h-11/12 flex flex-col justify-around mt-2">
+                    <label htmlFor="title" className="mb-4">
+                      title
+                    </label>
+                    <input
+                      name="title"
+                      type="text"
+                      className="border border-amber-800 mb-4 rounded h-1/12 pl-2"
+                      defaultValue={title}
+                    />
+                    <label htmlFor="description" className="mb-4">
+                      description
+                    </label>
+                    <textarea
+                      name="description"
+                      type="text"
+                      className="border border-amber-800 mb-4 rounded h-3/5 resize-none pl-2 pt-2"
+                      defaultValue={description}
+                    />
+                    <div className="flex justify-around">
+                      <button
+                        onClick={close}
+                        className="cursor-pointer rounded bg-red-400 hover:bg-red-600 text-white px-4 py-2 w-1/3"
+                      >
+                        Cancel
+                      </button>
+                      <button
+                        onClick={close}
+                        className="cursor-pointer rounded bg-blue-400 hover:bg-blue-600 text-white px-4 py-2 w-1/3"
+                      >
+                        Save
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </Modal>
+            ));
+          }}
+        >
+          edit
+        </button>
       )}
       <span className="text-xl font-bold">{move}</span>
       {title && <span>[{title}]</span>}
